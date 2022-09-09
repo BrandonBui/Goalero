186221f0bfb6c997e9e78948cd948cd701c147c2
Brandon Bui <33396985+BrandonBui@users.noreply.github.com>
2022-09-08 20:53:34 -0700

    Route edited
    If user is logged in they will not see the lanfding page, but if they
    are not logged in then they will see the landing page.

---
+++ lib/Authentication Pages/main_page2.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:goalero/landing_page.dart';
+
+import '../main.dart';
+import 'auth_page.dart';
+
+class MainPage2 extends StatelessWidget {
+  const MainPage2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return HomePage();
+            } else {
+              return LandingPage();
+            }
+          }),
+    );
+  }
+}
